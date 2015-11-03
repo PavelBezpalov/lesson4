167707f2094120d3b675cb40560cc3959ce0ffa5
Pavel Bezpalov <pavel.bezpalov@cyber-wizard.com>
2015-11-03 22:35:16 +0200

    Generate user model with scaffold, add bcrypt and shoulda-matchers gems, add user model validation specs

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
@@ -54,6 +54,7 @@ end
 group :test do
   # http://www.rubydoc.info/gems/capybara/2.5.0#Setup
   gem 'capybara'
+  gem 'shoulda-matchers', '~> 3.0'
 end
 
 # ruby version for Heroku
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103182822) do
+ActiveRecord::Schema.define(version: 20151103201603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,5 +26,15 @@ ActiveRecord::Schema.define(version: 20151103182822) do
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
 
 end
