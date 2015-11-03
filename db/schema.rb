ced225bd79df232516345148756cfd64367a5543
Pavel Bezpalov <pavel.bezpalov@cyber-wizard.com>
2015-11-03 20:17:40 +0200

    Add user_id column to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026111621) do
+ActiveRecord::Schema.define(version: 20151103182822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 20151026111621) do
     t.string   "tags"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
 end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  context 'user_id field' do
+    it 'exists' do
+      post = Post.create(title: 'First post',
+                          body: 'Body of the first post',
+                          tags: 'some, tags, here')
+      post.user_id = 7
+      expect(post.user_id).to eq(7)
+    end
+
+  end
+end
