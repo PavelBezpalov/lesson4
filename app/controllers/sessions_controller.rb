87933b0fd1c494e138ffdee73c37b8d397a7c815
Pavel Bezpalov <pavel.bezpalov@cyber-wizard.com>
2015-11-04 11:35:18 +0200

    Add session creating with specs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,13 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    user.authenticate(params[:password])
-    redirect_to posts_path
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = 'Your successfully logging in!'
+      redirect_to posts_path
+    else
+      flash[:error] ='There was a problem logging in. Please check your email and password.'
+      render action: 'new'
+    end
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -38,6 +38,51 @@ RSpec.describe SessionsController, type: :controller do
         expect(user).to receive(:authenticate)
         post :create, email: 'pavel.bezpalov@cyber-wizard.com', password: '12345678'
       end
+
+      it 'sets the user_id in the session' do
+        post :create, email: 'pavel.bezpalov@cyber-wizard.com', password: '12345678'
+        expect(session[:user_id]).to eq(user.id)
+      end
+
+      it 'sets the flash success message' do
+        post :create, email: 'pavel.bezpalov@cyber-wizard.com', password: '12345678'
+        expect(flash[:success]).to eq('Your successfully logging in!')
+      end
+    end
+
+    # Shared examples http://www.relishapp.com/rspec/rspec-core/v/3-3/docs/example-groups/shared-examples
+    shared_examples 'denied login' do
+      it 'renders the new template' do
+        post :create, email: email, password: password
+        expect(response).to render_template('new')
+      end
+
+      it 'sets the flash error message' do
+        post :create, email: email, password: password
+        expect(flash[:error]).to eq('There was a problem logging in. Please check your email and password.')
+      end
+    end
+
+    context 'without user credentials' do
+      let(:email) { '' }
+      let(:password) { '' }
+      it_behaves_like 'denied login'
+    end
+
+    context 'with not existed email' do
+      let(:email) { 'not.found@email.com' }
+      let(:password) { 'somepassword' }
+      it_behaves_like 'denied login'
+    end
+
+    context 'with incorrect password' do
+      let!(:user) { User.create(name: 'Pavel',
+                                email: 'pavel.bezpalov@cyber-wizard.com',
+                                password: '12345678',
+                                password_confirmation: '12345678') }
+      let(:email) { user.email }
+      let(:password) { 'incorrect' }
+      it_behaves_like 'denied login'
     end
   end
 end
